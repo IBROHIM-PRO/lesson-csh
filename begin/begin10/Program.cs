3ad1db63d66d3401492600be72193ded2464b3e9
ibrohim <kaldun597@gmail.com>
2025-10-07 00:07:34 +0500

    update begin10

--- begin/begin10/Program.cs
+++ begin/begin10/Program.cs
@@ -1,2 +1,4 @@
 ﻿int a = 2, b = 4;
-Console.WriteLine($" + = {a+b}; - = {a-b}; * = {a*b}; / = {a/d}");
+double powa = Math.Pow(a, 2);
+double powb = Math.Pow(b, 2);
+Console.WriteLine($" + = {powa+powb}; - = {powa-powb}; * = {powa*powb};");
