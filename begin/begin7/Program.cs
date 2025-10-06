84b1d7667906ae75d3518ef84716245142f9df07
ibrohim <kaldun597@gmail.com>
2025-10-06 23:48:33 +0500

    begin7

---
+++ begin/begin7/Program.cs
@@ -0,0 +1,6 @@
+﻿//Найти длину окружности L и площадь круга S заданного радиуса R:
+//L = 2*p*R, S = p*R^2
+int r = 4;
+const double p = 3.14;
+double l = 2 * p * r, d = p * Math.Pow(r, 2);
+Console.Writeline("L = " + l+" S = " + s);
