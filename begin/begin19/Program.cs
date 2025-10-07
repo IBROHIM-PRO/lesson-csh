cbc8ee6a882a49a3bff91aea19fd69d2cf204111
ibrohim <kaldun597@gmail.com>
2025-10-07 10:08:26 +0500

    Yoftani masohat va perimetri rostkunja bo kordinatahoi dodashuda

---
+++ begin/begin19/Program.cs
@@ -0,0 +1,4 @@
+﻿int x1 = 3, x2 = 6, y1 = 2, y2 = 7; //nektahoi porchaho
+double a = Math.Abs(x2 - x1); //tarafi ufuqi
+double b = Math.Abs(y2 - y1); //tarafi amudi
+Console.WriteLine($"P = {2 * (a + b)}; S = {a * b}"); //Perimetr va masohati rostkunja
