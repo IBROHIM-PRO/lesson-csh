699a01e6aac7fc0d2041e49c2e82c757fd91225d
ibrohim <kaldun597@gmail.com>
2025-10-07 09:59:26 +0500

    Yoftani darozii porchai AC * BC

---
+++ begin/begin18/Program.cs
@@ -0,0 +1,4 @@
+﻿int a = 4, b = 6, c = 8; // adadhoi dodashuda
+double ac = Math.Abs(c - a); //nektai AC
+double bc = Math.Abs(c - c); //nuktai BC
+Console.WrieteLine($"AC * BC = {ac*bc}"); //Darozii hosili zarbi porcha
